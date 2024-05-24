606d9520c63dcd23cb47f0eee20e513df67bd349
zarbo-ian <58311312+zarbo-ian@users.noreply.github.com>
2024-05-24 09:38:16 -0300

    fix funcion reportes

--- funciones.py
+++ funciones.py
@@ -8,6 +8,7 @@ id_publicacion_pub, usuario_publicador, publicacion = cargar_publicaciones(fo)
 
 def mostrar_reportes():
     is_valid_input = False
+    times_failed = 0
     while is_valid_input == False:
         nom_usuario = input("Ingrese un nombre de usuario: ")
         if nom_usuario in usuario_comentador or nom_usuario in usuario_publicador:
@@ -15,4 +16,8 @@ def mostrar_reportes():
         else:
             print("Nombre de usuario invalido. ", end= "")
             is_valid_input = False
+            times_failed += 1
+            if times_failed >= 3:
+                #volver al menú principal
+                pass
     #volver al menú principal
