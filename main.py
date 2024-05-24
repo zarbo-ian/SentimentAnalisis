87f33b45afdf42cb23171f0e7f9705008b15deae
Ctojo <ctojo@itba.edu.ar>
2024-05-24 08:34:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-import funciones as func
-import datos
+###########################
+####### FUNCION MENU ##########
+###########################
+def mostrar_menu():
+    print("\nMenú principal")
+    print("1. Carga de datos")
+    print("2. Análisis de la actividad de Influencers")
+    print("3. Reportes")
+    print("4. Salir")
+    n = input("Elige una opción (1-4): ")
+    return n
 
-print("Elija: Carga de datos, Análisis de la actividad de los influencers, Reportes, Salir")
+continuar = True
 
-comando = input()
-while comando != "Carga de datos" and comando != "Analisis" and comando != "Reportes" and comando != "Salir":
-    comando = input("Comando invalido, ingrese nuevamente: ")
+while continuar:
+    opcion = mostrar_menu()
 
-if comando == "Carga de datos":
-    pass
-elif comando == "Analisis":
-    pass
-elif comando == "Reportes":
-    pass
-elif comando == "Salir": 
-    pass
+    while opcion not in ["1", "2", "3", "4"]:
+        print("Opción no válida, por favor elige una opción del 1 al 4.")
+        opcion = mostrar_menu()
+
+    if opcion == "1":
+        print("Todo lo relacionado con datos")
+    elif opcion == "2":
+        print("Análisis detallado de la actividad de Influencers")
+    elif opcion == "3":
+        print("Generación de reportes")
+    elif opcion == "4":
+        print("Saliendo del programa...")
+        continuar = False
+    else:
+        continuar = True  # Opcional, para dejar claro que el bucle sigue si ninguna condición previa se cumple.
+
+    if opcion != "4" and continuar:
+        volver = input("¿Quieres volver al menú principal? (s/n): ")
+        if volver != 's':
+            print("Saliendo del programa...")
+            continuar = False
+
